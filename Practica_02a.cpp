a7aaa572fffcaefc8a057289dbf44672d58cd583
TucspDretcm <alexander.carpio@ucsp.edu.pe>
2022-04-17 13:30:06 -0500

    lst changue

--- Practica_02a.cpp
+++ Practica_02a.cpp
@@ -8,26 +8,22 @@ Alumno: Alexander Carpio Mamani
 División de enteros
 */
 
-#include <iostream>
-#include <bitset>
-using namespace std;
-
 int main(){
-	const int n_bits = 4;
-	int A=0, M=3, Q=7, Count=n_bits;
+	const int n_bits = 8;
+	int A=0, M=5, Q=253, Count=n_bits;
 	bitset<n_bits> binary_Q (Q), binary_A (A);
 	
 	while(Count>0){
 		binary_A <<= 1;
-		binary_A[0] = binary_Q[3];
+		binary_A[0] = binary_Q[n_bits-1];
 		A = binary_A.to_ulong();   // binario a psudo-entero
 		
-		binary_Q <<= 1;  // 101? => 0100
+		binary_Q <<= 1;  // 1011 => 0110
 		
 		A -= M;
 		
 		if (A >= 0){
-			binary_Q[0] = 1;  // 0100 => 0101
+			binary_Q[0] = 1;  // 0110 => 0111
 		}
 		else{
 			A += M;
@@ -35,9 +31,11 @@ int main(){
 		
 		bitset<n_bits> aux(A);
 		binary_A = aux;
-		Q = binary_Q.to_ulong();
 		Count--;
 	}
+	
+	Q = binary_Q.to_ulong();
+	
 	cout<<" Tipos\t\tBinario\t| Decimales"<<endl;
 	cout<<" Cociente\t"<<binary_Q<<"\t\t"<<Q<<endl;
 	cout<<" Residuo\t"<<binary_A<<"\t\t"<<A<<endl;
